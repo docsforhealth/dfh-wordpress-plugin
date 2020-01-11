2d69fb24e6a232b2a965416796a424996dff8ee8
Eric Bai <eric@ericbai.com>
2020-01-10 21:20:17 -0500

    tagged 0.0.15

--- docsforhealth.php
+++ docsforhealth.php
@@ -7,7 +7,7 @@
  * GitHub Plugin URI: ericbai/dfh-wordpress-plugin
  * Release Asset: true
  * Description: Custom plugin for the Docs for Health website
- * Version: 0.0.14
+ * Version: 0.0.15
  * License: Apache-2.0
  * License URI: https://www.apache.org/licenses/LICENSE-2.0.txt
  * Text Domain: dfh
